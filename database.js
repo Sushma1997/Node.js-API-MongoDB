fd79c0aa1f0addb033dcc3bf915f216f3896408d
Sushma Reddy T <sushee.reddy1997@gmail.com>
2025-07-10 18:08:54 -0500

    Update database.js

--- database.js
+++ database.js
@@ -1,1 +1,15 @@
-const 
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/mydatabase', {
+  useNewURLParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'error connecting to MongoDB:'));
+db.once('open', () => {
+  console.log('successfully connected to MongoDB');
+});
+
+module.exports = mongoose;
